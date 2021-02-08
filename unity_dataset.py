5454ed3dd4aa9aec3774628b66dc2cfc56a28e63
Evan Griffiths <evangriffiths@hotmail.co.uk>
2021-02-08 17:52:46 +0000

    ✨ New model, which is trained with a scene render and its corresponding depth buffer

--- unity_dataset.py
+++ unity_dataset.py
@@ -24,28 +24,38 @@ class UnityDataset(Dataset):
     def __getitem__(self, index):
 
         patch = self.patch_from_index(index)
+        # patch[0] = image index
+        # patch[1] = col that we'll take the patch from
+        # patch[2] = row that we'll take the patch from
 
-        x_filename = "{}.png".format(patch[0] + self.num_images)
-        y_filename = "{}.png".format(patch[0])
+        x_reg_filename = "regular/{}.png".format(patch[0] + self.num_images)
+        x_dep_filename = "depth/{}.png".format(patch[0])
+        y_filename = "regular/{}.png".format(patch[0])
 
-        x_img_path = os.path.join(self.root_dir, x_filename)
+        x_reg_path = os.path.join(self.root_dir, x_reg_filename)
+        x_dep_path = os.path.join(self.root_dir, x_dep_filename)
         y_img_path = os.path.join(self.root_dir, y_filename)
 
-        x_image = io.imread(x_img_path)
+        x_reg_image = io.imread(x_reg_path)
+        x_dep_image = io.imread(x_dep_path)
         y_image = io.imread(y_img_path)
 
         patch_x_bounds = patch[1]*self.patch_size[0], (patch[1]+1)*self.patch_size[0]
         patch_y_bounds = patch[2]*self.patch_size[1], (patch[2]+1)*self.patch_size[1]
 
         # Get rid of alpha channel and crop to desired patch size
-        x_image = x_image[patch_y_bounds[0]:patch_y_bounds[1], patch_x_bounds[0]:patch_x_bounds[1], :3]
+        x_reg_image = x_reg_image[patch_y_bounds[0]:patch_y_bounds[1], patch_x_bounds[0]:patch_x_bounds[1], :3]
         y_image = y_image[patch_y_bounds[0]:patch_y_bounds[1], patch_x_bounds[0]:patch_x_bounds[1], :3]
 
+        # Get rid of all but one channel (greyscale) and crop to desired patch size
+        x_dep_image = x_dep_image[patch_y_bounds[0]:patch_y_bounds[1], patch_x_bounds[0]:patch_x_bounds[1], :1]
+
         if self.transform:
-            x_image = self.transform(x_image)
+            x_reg_image = self.transform(x_reg_image)
+            x_dep_image = self.transform(x_dep_image)
             y_image = self.transform(y_image)
 
-        return x_image, y_image
+        return x_reg_image, x_dep_image, y_image
 
     def patch_from_index(self, index):
         patches = self.patch_dim[0] * self.patch_dim[1]
